724fc6e69e458a57335b2af3addb0275b8e374cc
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-07-11 11:26:34 +0100

    better automatisation

---
+++ stalwart/setup-server.sh
@@ -0,0 +1,36 @@
+export ENDPOINT=https://mail.benkner-it.com
+export PASSWORD=your-password
+alias stalwart-cli='docker exec -ti mailserver stalwart-cli'
+
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config certificate.default.cert "%{file:/data/certs/mail.example.com/cert.pem}%"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config certificate.default.default true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config certificate.default.private-key "%{file:/data/certs/mail.example.com/key.pem}%"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.hostname "mail.benkner-it.com"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config http.hsts true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config http.permissive-cors false
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config http.url "protocol + '://' + config_get('server.hostname') + ':' + local_port"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config http.use-x-forwarded true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.http.bind "[::]:8080"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.http.protocol "http"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.https.bind "[::]:443"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.https.protocol "http"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.https.tls.implicit true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.bind "[::]:993"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.protocol "imap"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.proxy.override true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.proxy.trusted-networks.0 "172.19.0.2"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.proxy.trusted-networks.1 "172.19.0.0/16"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.imaptls.tls.implicit true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.smtp.bind "[::]:25"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.smtp.protocol "smtp"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.smtp.proxy.override true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.smtp.proxy.trusted-networks.0 "172.19.0.2"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.smtp.proxy.trusted-networks.1 "172.19.0.0/16"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.bind "[::]:465"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.protocol "smtp"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.proxy.override true
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.proxy.trusted-networks.0 "172.19.0.2"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.proxy.trusted-networks.1 "172.19.0.0/16"
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server add-config server.listener.submissions.tls.implicit true
+
+stalwart-cli -u ${ENDPOINT} -c ${PASSWORD} server reload-config
